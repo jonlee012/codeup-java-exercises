c6760dd603fce322dc2a73057d3cfed29eb1ac32
jonlee012 <jonlee86.jl@gmail.com>
2022-05-18 17:26:05 -0500

    helloworld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,36 @@
 public class HelloWorld {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int myFavoriteNumber = 3;
+        String myString = "boom";
+        System.out.println(myString);
+        System.out.println(myFavoriteNumber);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        String class = "hi";
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+//        int x = 4;
+//        int x = 4 + 5;
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+//        int y = 4 * 3;
+//        System.out.println(y);
+//         int x = 10;
+//         int y = 2;
+        int x = 10 / 2;
+        int y = 2 - 10;
+        System.out.println(x);
+        System.out.println(y);
+
+
     }
 }
