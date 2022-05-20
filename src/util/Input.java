c311b180d119ea2326492e8680e6911a0b7b4616
jonlee012 <jonlee86.jl@gmail.com>
2022-05-20 11:12:57 -0500

    util

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,2 +1,20 @@
-package util;public class Input {
+package util;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner;
+
+    public String getString() {
+        System.out.println("hello");
+        return scanner.nextLine();
+    }
+    public Boolean yesNo(){
+        if (Objects.equals(scanner.nextLine(), "yes")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+
