557d2a090d6c762a881dc9ceb0b88bfc284c83a1
jonlee012 <jonlee86.jl@gmail.com>
2022-05-20 16:50:53 -0500

    input

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,2 +1,17 @@
-package util;public class InputTest {
+package util;
+
+public class InputTest {
+    public static void main(String[] args) {
+//        System.out.println(getString());
+        Input in = new Input();
+        System.out.println("Give me a string!");
+        String storedString = in.getString();
+
+        System.out.println(storedString);
+
+        System.out.println("yes or no? (yes/no)");
+        Boolean myChoice = in.yesNo();
+
+        System.out.println(myChoice);
+    }
 }
