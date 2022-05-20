cbfdae43daa74daafef6834353b12dd2da039a85
jonlee012 <jonlee86.jl@gmail.com>
2022-05-20 16:50:38 -0500

    input

--- src/util/Input.java
+++ src/util/Input.java
@@ -3,18 +3,47 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Input {
-    private Scanner scanner;
+
+
+
+    private Scanner scanner = new Scanner(System.in);
 
     public String getString() {
         System.out.println("hello");
         return scanner.nextLine();
     }
-    public Boolean yesNo(){
+
+    public Boolean yesNo() {
         if (Objects.equals(scanner.nextLine(), "yes")) {
             return true;
         } else {
             return false;
         }
     }
+
+    public int getInt(int min, int max) {
+        System.out.println("get lowest num: " + min + " and highest num: " + max);
+        int newNum;
+        newNum = this.scanner.nextInt();
+        try {
+            return Integer.parseInt((getString()));
+        } catch (NumberFormatException e) {
+            System.err.println("you've reach an error.");
+        }
+        if (newNum <= max && newNum >= min) {
+            System.out.println("that's a valid number");
+            return newNum;
+        } else {
+            System.out.println("please add a number between inputs");
+            return getInt(min, max);
+        }
+    }
+
+    public int getInt() {
+        return scanner.nextInt();
+    }
+
 }
+
+
 
