4f9256b17d8b3e2eb556f284203cd864cb601ade
jonlee012 <jonlee86.jl@gmail.com>
2022-05-23 11:29:30 -0500

    warmups

---
+++ src/Warmups.java
@@ -0,0 +1,22 @@
+public class Warmups {
+    //    Let's create a method that returns
+//    the sum of all integers in an int array (nums)
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int i : numbers)
+            sum += i;
+            System.out.println(sum);
+
+
+//        for (int n : numbers) {
+//            count += n;
+//            System.out.println(n);
+//        }
+//        return Arrays.stream(numbers).sum();
+//        System.out.println(numbers);
+
+    }
+
+}
