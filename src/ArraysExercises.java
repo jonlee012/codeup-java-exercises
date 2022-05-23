b328314d001d7d6c79bc8c3359c70a30020354cf
jonlee012 <jonlee86.jl@gmail.com>
2022-05-23 14:59:43 -0500

    array

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class ArraysExercises {
+import java.util.Arrays;
+
+public class ArraysExercises {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(numbers);
+        //arrays.tostring is necesssary to print all elements
+        System.out.println(Arrays.toString(numbers));
+
+        String[] persons = {"Jon Jones", "Holy Moly", "Selma Smith"};
+        for (String person : persons) {
+            System.out.println(person);
+        }
+        String[] people = Arrays.copyOf(persons, 4);
+//        add in a new person to existing array
+        people[3] = "Busta Rhymes";
+        System.out.println(Arrays.toString(people));
+
+//    public static void numArr(){
+//
+//
+//    }
+
+    }
+    String  person;
+    public  void addPerson (String name) {
+        person = name;
+    }
 }
