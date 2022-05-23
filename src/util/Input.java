8cf4957301c162eeae83fe73a84990ee7d935a79
jonlee012 <jonlee86.jl@gmail.com>
2022-05-23 10:01:58 -0500

    input

--- src/util/Input.java
+++ src/util/Input.java
@@ -42,6 +42,30 @@ public class Input {
     public int getInt() {
         return scanner.nextInt();
     }
+    public double getDouble(double min, double max) {
+        System.out.println("Enter an integer between " + min + " and " + max);
+
+        double userInput = this.scanner.nextDouble();
+
+        try {
+            return Double.parseDouble(getString());
+        }
+        catch (NumberFormatException e){
+            System.err.println("ERROR: Not a valid input, try again.");
+        }
+
+        while (userInput <= min || userInput >= max) {
+            System.out.println("Enter an integer between " + min + " and " + max);
+            userInput = this.scanner.nextDouble();
+        }
+
+        System.out.println("Thank you for the valid number!");
+        return userInput;
+    }
+
+    public double getDouble(){
+        return scanner.nextDouble();
+    }
 
 }
 
