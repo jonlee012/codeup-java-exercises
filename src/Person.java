e3ad3ddc07049a75dc2909f7829cca4db6a1295f
jonlee012 <jonlee86.jl@gmail.com>
2022-05-24 14:05:41 -0500

    person

--- src/Person.java
+++ src/Person.java
@@ -1,49 +1,47 @@
+package util;
+
 public class Person {
 
-    // The class has a private name property that is a string
-    public String name;
-    public int age;
+    private String name;
+    protected int age;
 
-    //The class should have a constructor that accepts a String value and sets the person's name to the passed string.
-    public Person(String name) {
+    public Person(){}; //Default constructor
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public Person(String name, int age){
         this.name = name;
         this.age = age;
     }
 
-    // returns the person's name
     public String getName(){
-        return this.name;
+        //TODO: return the person's name
+        return name;
     }
 
-    // changes the name property to the passed value
     public void setName(String name){
+        //TODO: change the name property to the passed value
         this.name = name;
     }
 
-    // prints a message to the console using the person's name
     public void sayHello(){
-        System.out.println("Hi, my name is: " + this.name);
-    }
-    // prints a message to the console using the person's age
-    public int getAge(){
-//        System.out.println("Hi, my age is: " + this.age);
-//                this.age = 35;
-        return this.age;
-    } public void setAge(int age){
-        this.age = age;
+        //TODO: print a message to the console using the person's name
+        System.out.println("Hello, " + name);
     }
 
     // Create a main method on the class that creates a new Person object and tests the above methods.
     public static void main(String[] args) {
-        Person John = new Person("John");
+        Person Jon = new Person("Jon");
         // Rewrite name to add last name, this allows us to test.test getName() and setName()
-        John.setName(John.getName() +  " Lee");
+        Jon.setName(Jon.getName() +  " Lee");
         // Test new name
-        John.sayHello();
+        Jon.sayHello();
         // Test age
-        John.getAge();
+//        John.getAge();
 //        set age
-        John.setAge(John.getAge() + 35);
+//        John.setAge(John.getAge() + 35);
         // 2. Understanding references
         Person person1 = new Person("Kim");
         Person person2 = new Person("Kim");
