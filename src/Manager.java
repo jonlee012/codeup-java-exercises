d1476933103dfb078edfaf35587f778d591313ce
jonlee012 <jonlee86.jl@gmail.com>
2022-05-24 15:31:04 -0500

    manager

--- src/Manager.java
+++ src/Manager.java
@@ -1,7 +1,32 @@
-public class Manager extends Worker {
+//import util.Person;
+//
+//public class Manager extends Employee {
+//    public Manager(String managerName) {
+//        super(managerName);
+//    }
+//    public String work() {
+//        return "Managing employees";
+//    }
+//
+//    public static void main(String[] args) {
+//        Manager Kevin = new Manager("Kevin");
+//
+//        Kevin.sayHello();
+////        Kevin.sayHello();
+////        System.out.println("jon.name = " + jon.getName());
+//
+//        Kevin.doWork();
+//
+//        System.out.println();
+//        Person newManager = new Person("Bobby");
+//
+//        newManager.sayHello();
+//    }
+//}
 
-    public String work() {
-        return "Managing employees";
-    }
-
-}
+//public class Manager extends Employee{
+//
+//    public String work() {
+//        return "managing employees increasing efficiency";
+//    }
+//}
