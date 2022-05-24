c2c13a472ed277d478e21aafb07a12b87ff1700e
jonlee012 <jonlee86.jl@gmail.com>
2022-05-24 13:53:23 -0500

    manager

--- src/Manager.java
+++ src/Manager.java
@@ -1,5 +1,7 @@
-//public class Manager extends Employee {
-//    public String work() {
-//        return "Managing employees";
-//    }
-//}
+public class Manager extends Worker {
+
+    public String work() {
+        return "Managing employees";
+    }
+
+}
