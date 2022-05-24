948c7f7286966b9afc054c3cce1a414fc15cbdf9
jonlee012 <jonlee86.jl@gmail.com>
2022-05-24 15:31:50 -0500

    employee

--- src/Employee.java
+++ src/Employee.java
@@ -1,14 +1,37 @@
-//class Employee extends Person {
-//    public String getName() {
-//        return this.name;
-//    }
-//
-//    public int getAge() {
-//        // ERROR!
-//        return this.age;
-//    }
-//
-//    public static void main(String[] args) {
-//
-//    }
-//}
+import util.Person;
+
+public class Employee extends Person {
+
+    public Employee(String employeeName, int age) {
+        super(employeeName, age);
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void sayHello() {
+        System.out.println("Welcome to the Codeup, " + this.name + ", how can I help you?"); //Overriding method
+    }
+
+    public void doWork() {
+        System.out.println("Are you done with work, " + this.name + " ?");
+    }
+
+
+    public static void main(String[] args) {
+        Person unemployed = new Person("Seth");
+        Employee jon = new Employee("jon", 35);
+
+        unemployed.sayHello();
+        jon.sayHello();
+        System.out.println("name = " + jon.getName());
+        System.out.println(jon.getName() + ", " + jon.getAge());
+        jon.doWork();
+//        System.out.println("unemployed.name = " +  unemployed.getName());
+
+//        unemployedJoe.doWork();
+
+    }
+
+}
