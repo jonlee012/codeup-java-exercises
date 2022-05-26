2cb79eac7224192863e81faaf337f1ef92d04e65
jonlee012 <jonlee86.jl@gmail.com>
2022-05-26 14:42:14 -0500

    shapes

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -4,8 +4,8 @@ public class ShapesTest {
 
     public static void main(String[] args) {
 
-        Measureable myShape = new Rectangle(4, 5);
-        Measureable myShape2 = new Square(5);
+        Measurable myShape = new Rectangle(3, 5);
+        Measurable myShape2 = new Square(5);
 
         System.out.println(myShape.getArea());
         System.out.println(myShape2.getArea());
