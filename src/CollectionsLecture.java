ebc5a6fed6a7369e262a773e50fbd873b046fff9
jonlee012 <jonlee86.jl@gmail.com>
2022-05-26 11:23:39 -0500

    collections

--- src/CollectionsLecture.java
+++ src/CollectionsLecture.java
@@ -5,7 +5,34 @@ import java.util.List;
 
 public class CollectionsLecture {
     public static void main(String[] args) {
+// We'll start by defining an array list and adding some items to it
+        ArrayList<String> roasts = new ArrayList<>();
+        roasts.add("light");
+        roasts.add("medium");
+        roasts.add("medium");
+        roasts.add("dark");
 
+        System.out.println(roasts); // [light, medium, medium, dark]
+
+// using .contains
+        roasts.contains("dark"); // true
+        roasts.contains("espresso"); // false
+
+// using .lastIndexOf
+        roasts.lastIndexOf("medium"); // 2
+
+// using .isEmpty
+        roasts.isEmpty(); // false
+        ArrayList<String> emptyList = new ArrayList<>();
+        emptyList.isEmpty(); // true
+
+// .remove has two variants
+        roasts.remove("espresso"); // false
+        roasts.remove("medium"); // true
+        System.out.println(roasts); // [light, medium, dark]
+
+        roasts.remove(2); // "dark"
+        System.out.println(roasts); // [light, medium]
 
 
 
