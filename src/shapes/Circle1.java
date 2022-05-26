a60bd60c94880febaad234e808cc71b307172751
jonlee012 <jonlee86.jl@gmail.com>
2022-05-26 11:23:03 -0500

    circles

--- src/shapes/Circle1.java
+++ src/shapes/Circle1.java
@@ -1,2 +1,19 @@
-package shapes;public class Circle1 {
+package shapes;
+import util.Input;
+
+
+public class Circle1 {
+
+    public static void circle() {
+        Input input = new Input();
+        double in = input.getDouble(1, 1000);
+        Circle circle = new Circle(in);
+        System.out.println("area = " + circle.getArea());
+        System.out.println("Circumference = " + circle.getCircumference());
+    }
+
+    public static void main(String[] args) {
+        circle();
+    }
+
 }
