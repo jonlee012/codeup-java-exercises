abd50ab5a9bd6ef1f63b6184473f151289056206
jonlee012 <jonlee86.jl@gmail.com>
2022-05-26 14:41:53 -0500

    shapes

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,2 +1,27 @@
-package shapes;public class Quadrilateral {
+package shapes;
+
+abstract class Quadrilateral extends Shape implements Measurable {
+
+    protected double length;
+    protected double width;
+
+    // constructor
+    public Quadrilateral(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    // getters
+    public double getLength() {
+        return this.length;
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    //setters
+    abstract void setLength(double length);
+
+    abstract void setWidth(double width);
 }
