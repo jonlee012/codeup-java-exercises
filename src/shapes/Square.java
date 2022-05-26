3491ce4a384dd3208bfdf669b1ebc715ed3ebbf1
jonlee012 <jonlee86.jl@gmail.com>
2022-05-26 14:42:25 -0500

    shapes

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,2 +1,28 @@
-package shapes;public class Square {
+package shapes;
+
+public class Square extends Quadrilateral{
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public double getArea() {
+        return this.length * this.width;
+    }
+
+    public double getPerimeter() {
+        return 2 * (this.length + this.width);
+    }
+
+    public void setLength(double side) {
+        this.length = side;
+    }
+
+    public void setWidth(double side) {
+        this.width = side;
+    }
+//    public void setSide(int side){
+//        this.side = side;
+//        this
+//    }
 }
