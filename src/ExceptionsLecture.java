861069a02f6f3bc2be5aa9b111717fa57b323f91
jonlee012 <jonlee86.jl@gmail.com>
2022-05-31 11:02:40 -0500

    input test' git push origin main git commit -m input '

--- src/ExceptionsLecture.java
+++ src/ExceptionsLecture.java
@@ -3,10 +3,14 @@ public class ExceptionsLecture {
 
     public static void main(String[] args) {
         try {
+            //checked exception
+            //pre built with java
             int[] numbers = {1, 2, 3};
             System.out.println(numbers[999]);
         } catch (Exception e) {
 //        System.out.println(numbers[1]);
+
+            //custom exception
 //        throw new Exception("Something bad happened!");
         System.out.println("doesn't work");
         }
