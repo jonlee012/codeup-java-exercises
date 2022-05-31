32f53ad9611bc0c2e43a1c30b854c0e651e432c4
jonlee012 <jonlee86.jl@gmail.com>
2022-05-31 10:30:04 -0500

    exceptions

--- src/ExceptionsLecture.java
+++ src/ExceptionsLecture.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class ExceptionsLecture {
+public class ExceptionsLecture {
+
+
+    public static void main(String[] args) {
+        try {
+            int[] numbers = {1, 2, 3};
+            System.out.println(numbers[999]);
+        } catch (Exception e) {
+//        System.out.println(numbers[1]);
+//        throw new Exception("Something bad happened!");
+        System.out.println("doesn't work");
+        }
+    }
 }
