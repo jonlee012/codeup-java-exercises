a6489a7277a2ac879771380543fc0d1c16a092fe
jonlee012 <jonlee86.jl@gmail.com>
2022-09-26 12:50:33 -0500

    reverse split string practice

--- src/ReverseSplit.java
+++ src/ReverseSplit.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class ReverseSplit {
+import java.util.Scanner;
+
+public class ReverseSplit {
+
+    public static void main(String[] args) {
+//        set a str variable
+        String str;
+//        scanner to receive user input
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your String");
+//        assign scanner input to str
+        str = in.nextLine();
+//        split the string with built in method
+        String[] token = str.split("");
+//        in reverse
+        for(int i = token.length-1; i >= 0; i--) {
+            System.out.print(token[i] + " ");
+        }
+//        split normal
+        for(int i=0; i < token.length; i++) {
+            System.out.print(token[i] + " ");
+        }
+    }
 }
